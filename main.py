5efc620daa748715ccdcae4fbdcf975f15b45757
icodeologist <denzilpinto01@gmail.com>
2025-01-27 10:45:49 +0530

    FPS system and less cpu use

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen=pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-    print("Starting asteroids!")
+    dt = 0
+    #change in the time or delta
+    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         pygame.Surface.fill(screen ,(0,0,0))
         pygame.display.flip()
-
+        #tick also return the time between different frames
+        time_diff_between_frames = clock.tick(60)
+        dt = time_diff_between_frames/1000
+        print(dt)
 if __name__ == "__main__":
     main()
